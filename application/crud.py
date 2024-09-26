6ead6de7786b3a0a2179e56eec989876b8595619
Ikbal Azimari Touré <azimari.toure.etu@esmt.sn>
2024-09-26 13:32:22 +0000

    added author

--- application/crud.py
+++ application/crud.py
@@ -106,6 +106,7 @@ def get_discussions(db: Session):
 def get_discussion(db: Session, discussion_id: int):
     discussion = db.query(models.Discussion).filter(models.Discussion.id == discussion_id).first()
     messages = db.query(models.Message).filter(models.Message.id_discussion == discussion_id).all()
+    author = db.query(models.ForumUser).filter(models.ForumUser.id == discussion.id_utilisateur).first()
     finalMessages = []
 
     for message in messages:
@@ -128,6 +129,7 @@ def get_discussion(db: Session, discussion_id: int):
     return {
         "id": discussion.id,
         "titre": discussion.titre,
+        "auteur": author,
         "sous_titre": discussion.sous_titre,
         "contenu": discussion.contenu,
         "creation": discussion.creation,
